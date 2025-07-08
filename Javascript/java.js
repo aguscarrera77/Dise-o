ca0e2aeecc974ecee2c83ce059d124f392bc3e1f
aguscarrera77 <aguscarrera77@gmail.com>
2025-07-08 16:38:23 -0300

    Actual

---
+++ Javascript/java.js
@@ -0,0 +1,20 @@
+/*let edad= 47;
+let nombre= 'agustin';
+console.log(edad);
+console.log(nombre);
+
+alert('Bienvenido al curso de Diseno Web');
+prompt('como se llama usted');*/
+
+let nombre='Lucas';
+ console.log('Hola '+ nombre +'. Como estas? ');
+
+ let usuario=prompt('Como te llamas');
+
+ alert('Hola'+' '+ usuario +' Bienvenido a mi pagina.');
+ 
+ let color1= prompt('Elija un color');
+ let color2=prompt('Elija otro color');
+
+ console.log('Si mezclas '+ color1+' y '+color2+' No respetas la formula 60 30 10');
+
