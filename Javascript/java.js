7cd82099a5d181c3a6a078c3aa956d2664966dc0
aguscarrera77 <aguscarrera77@gmail.com>
2025-07-10 23:01:58 -0300

    Actual

--- Javascript/java.js
+++ Javascript/java.js
@@ -5,7 +5,7 @@ console.log(nombre);
 
 alert('Bienvenido al curso de Diseno Web');
 prompt('como se llama usted');*/
-
+/*
 let nombre='Lucas';
  console.log('Hola '+ nombre +'. Como estas? ');
 
@@ -16,5 +16,59 @@ let nombre='Lucas';
  let color1= prompt('Elija un color');
  let color2=prompt('Elija otro color');
 
- console.log('Si mezclas '+ color1+' y '+color2+' No respetas la formula 60 30 10');
+ console.log('Si mezclas '+ color1+' y '+color2+' No respetas la formula 60 30 10');*/
+/* uso dos prompt para sumar sus valores y que se vean en el console.log y el parseInt para asegurarme que ingrese numeros enteros
+ let number1= parseInt(prompt('Ingrese el primer numero'));
+ let number2= parseInt(prompt('Ingrese el segundo numero'));
 
+ alert('La suma de los numeros ingresados: '+(number1*number2));
+
+if ciclo que depende de un condicional y la sintaxis es:
+if(condicional){
+codigo.
+}
+si es true se imprime y si es false no se imprime el ciclo.*/
+
+let age=Number(prompt('que edad tenes'));
+
+if(age>=18){
+alert('Acceso Permitido.');  
+}
+else{
+alert('Usted vayase a otra pagina.');
+
+}
+
+/* genero 2 variables con prompt una valido el usario y la otra password*/
+
+let user=prompt('Ingrese su usuario');
+let password= prompt('Ingreso su password');
+
+if(user!=="" && password!==''){
+
+console.log('Usted a ingresado a su perfil.')
+
+}
+else{
+    alert('Complete sus datos.');
+}
+
+let saber= prompt('Usted sabe de Javascript?Si/No');
+
+if(saber==='Si'){
+console.log('Usted puede ingresar al curso');
+}
+else if(saber==='No'){
+
+ console.log('Hago un curso de ingreso');
+}
+else{alert('Ingresa la palabra correcta.')};
+
+/*Comienzo del ciclo for*/
+let numero= prompt('Que tabla de multiplicar queres ver');
+
+for(let i=0; i<=10;i++){
+
+console.log(numero+'x'+i+'='+(numero*i));
+
+}
